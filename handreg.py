8caae4af84f324b4b0c52674c2168e94c2d3b3ac
doommot <34127346+doommot@users.noreply.github.com>
2017-12-03 19:36:41 +0300

    registration by hands via cmd

---
+++ handreg.py
@@ -0,0 +1,23 @@
+from account import account
+import config
+from datetime import datetime
+
+def log(line):
+	log_stream = open(config.logfile, "a", encoding = 'utf8')
+	mem = 'HREG:' + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S;") + line+'\n'
+	log_stream.write(mem)
+	log_stream.close()
+
+accounts = []
+for x in range (int(input("How many accs to reg?\n"))):
+	accounts.append(account())
+	log("account reg"+str(x))
+	accounts[x].reg(input("Enter phone:\n"))
+
+
+def save_accounts_list(accounts):#!this will rewrite current list
+	log("saving account list")
+	f=open(config.accountfile,'w')#filename must be in class fields
+	for ele in l1:
+		f.write(ele+'\n')
+	f.close()
