6bcb8c776da5ea77396fd4b648b9fd0c836e0f5e
doommot <34127346+doommot@users.noreply.github.com>
2017-12-09 17:08:11 +0300

    v0.4
    Deleted private keys.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 136379
-api_hash = '99f3a6dd52cf8c770bed8d601f17bdad'
+api_id = #your api_id
+api_hash = ''#your api_hash key
 logfile = "log.txt"
 accountfile="accounts.txt"
 
-bot_token = '359393059:AAEi7Q5yKiDHerSVqzpsHuhCxC189Vx9kSQ'
+bot_token = ''#your token
